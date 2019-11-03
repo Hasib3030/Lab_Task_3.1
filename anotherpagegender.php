4ffb8ddde30908164874e94cd7f1e80faa16311a
Hasib Ahmed <55645743+Hasib3030@users.noreply.github.com>
2019-11-03 10:25:24 +0600

    Add files via upload

---
+++ anotherpagegender.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+Gender Name
+</head>
+<body>
+    <form method="POST" action="a.php">
+        <fieldset>
+            <legend> GENDER </legend>
+            <input type="radio" name="gender" value="Male"> MALE
+            <input type="radio" name="gender" value="Female"> FEMALE
+            <input type="radio" name="gender" value="Female"> OTHER<br>
+            <input type="submit" name="submit" value="Submit">
+        </fieldset>
+    </form>
+</body>
+</html>
+
+<?php
+    
+    if(isset($_POST['submit']))
+    {
+        if(isset($_POST['gender']))
+        { echo " Your Gender Is: ".$_POST['gender'];}
+        else
+        {echo "Mark Your Gender !"; }
+    }
+    
+?>
